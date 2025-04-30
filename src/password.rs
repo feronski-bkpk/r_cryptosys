adbadec8b3c69411f91e35a437e4e8103ce510c3
Feronski <feronski.bkpk@gmail.com>
2025-04-30 13:31:18 +0300

    bugfix

--- src/password.rs
+++ src/password.rs
@@ -1,21 +1,12 @@
 use std::io::{self, Write};
 use crate::error::CryptoError;
 
-#[cfg(unix)]
-extern crate libc;
-
 pub fn read_password(prompt: &str) -> Result<String, CryptoError> {
     print!("{}", prompt);
     io::stdout().flush()?;
 
-    #[cfg(unix)]
-    let _ = unsafe { libc::system("stty -echo") };
-
     let mut password = String::new();
     io::stdin().read_line(&mut password)?;
-
-    #[cfg(unix)]
-    let _ = unsafe { libc::system("stty echo") };
 
     if password.ends_with('\n') {
         password.pop();
